9442b362d63259b0f12f13fdec2eecbc0ccf1b7b
LUKE-LAPTOP\Luke <luke.melville@gmail.com>
2018-09-24 11:19:34 +0100

    Game Manager added with Start and Quit

---
+++ Assets/Scripts/cs_GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class cs_GameManager : MonoBehaviour {
+
+    private enum e_GameState
+    {
+        Paused,
+        GameOn
+    }
+
+    private e_GameState gs_CurrentGameState  = e_GameState.Paused;
+    public GameObject g_StartPanel;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Func to begin game
+    // Todo - main Update loop only runs if we are checking Game on
+    public void StartGame()
+    {
+        gs_CurrentGameState = e_GameState.GameOn;
+        g_StartPanel.SetActive(false);
+    }
+}
