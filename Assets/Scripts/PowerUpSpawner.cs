6db0155f65756eac34a4441193ebc89e7541f4bb
Josh <josh.windsor.mail@gmail.com>
2018-09-24 15:51:26 +0100

    pickups

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> spawnPoints;
+
+    [SerializeField]
+    private List<GameObject> powerUps;
+
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10)
+        {
+            SpawnObject();
+            timer = 0;
+        }
+    }
+
+    private void SpawnObject()
+    {
+        int r = Random.Range(0, spawnPoints.Count);
+        int a = Random.Range(0, powerUps.Count);
+        Instantiate(powerUps[a], spawnPoints[r].transform.position, Quaternion.identity);
+    }
+}
